e7d6a651e5e0522aa9a4ce664ce6166991197eb2
Leo G <leoko4433@gmail.com>
2018-03-17 09:48:49 +0100

    [~] Reading map data from sql

--- public/js/map.js
+++ public/js/map.js
@@ -19,6 +19,7 @@ function initMap() {
       });
 
       locate();
+      fetchMapData();
 }
 
 function locate() {
@@ -31,6 +32,43 @@ function locate() {
 
   map.setCenter({lat: 51.448006, lng: 5.454005});
   map.setZoom(16);
+}
+
+function fetchMapData() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "http://37.252.184.84:3000/requests", true);
+  xhttp.addEventListener('load', function() {
+    var response = JSON.parse(xhttp.responseText);
+
+    for (entry of response) {
+
+      let info = new google.maps.InfoWindow({
+        content:"<div class='mapEntry'>"+
+                  "<h1>"+entry.requested_resource+"</h1>"+
+                  "<p><i>Pete is in need of something!</i><br>"+
+                  "<b>Needed: </b>"+entry.requested_resource+"<br>"+
+                  "<b>Location: </b>"+entry.location+"</p>"+
+                  "<a class='waves-effect waves-light btn'>Open in Google Maps</a></div>"
+      });
+
+      var diff = moment(new Date(entry.createdAt)).fromNow();
+
+      let marker = new google.maps.Marker({
+        position: {lat: Number(entry.latitude), lng: Number(entry.longitude)},
+        // icon: {url:"", scaledSize: new google.maps.Size(30, 30), labelOrigin:{x: 15, y: 40}},
+        label: {color: "#2b00f7", fontWeight:"bold", text:diff,  labelOrigin:{x: 15, y: 40}},
+        map: map
+      });
+
+      marker.addListener('click', function() {
+        info.open(map, marker);
+      });
+    }
+  });
+
+  xhttp.addEventListener('error', () => console.log("Request failed"));
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send();
 }
 
 function showPosition(position) {
